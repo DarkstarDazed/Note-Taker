f4705007652344e5c394d0a4b50a21473cc0108c
mediazjr <mediazjr@gmail.com>
2022-01-01 11:15:44 -0800

    added server.js with dependencies and get/post routes

---
+++ server.js
@@ -0,0 +1,44 @@
+
+// dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const noteList = require('./db/db.json');
+// server setup
+const PORT = process.env.PORT || 3001;
+const app = express();
+// express data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// html routes and get/post requests
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteList);
+});
+
+app.post("/api/notes", (req, res) => {
+    req.body.id = noteList.length.toString();
+    const newNote = req.body;
+    createNewNote(newNote, noteList);
+    res.json(newNote);
+});
+
+
+
+
+
+
+// listen for connections
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
