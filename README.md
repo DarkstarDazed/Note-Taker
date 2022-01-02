f67d923f34f480b48609e1faa05ff7bf53fa6490
mediazjr <mediazjr@gmail.com>
2022-01-02 14:29:41 -0800

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Note-Taker
+
+## Descrption:
+
+Using express.js as my backend, I built a simple note taker using javascipt that allows the user to input title text and note text. When saving the note, it populates a list on the side with other saved notes. 
+
+To use the app, run npm i in your teminal followed by node server.js to spin up the server. Navigate to http://localhost:3001 in your browser and start taking notes.  It is accesible via Heroku as well (see link below). 
+
+## Screenshot:
+
+
+## Deployed App:
+
+[Heroku App](https://lit-beyond-14267.herokuapp.com/)
+
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const noteList = require('./db/db.json');
+
 // server setup
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // express data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+
 // html routes and get/post requests
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -30,12 +33,14 @@ app.post("/api/notes", (req, res) => {
     createNewNote(newNote, noteList);
     res.json(newNote);
 });
+
 // function to add new note to list
 function createNewNote(body, notes) {
     const note = body;
     notes.push(note);
     fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(noteList, null, 2));
 }
+
 // listen for connections
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
